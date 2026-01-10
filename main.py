8512d6194bccbbfee526dfbc681d697f8fda866d
Dowiw <monjie12272@gmail.com>
2026-01-10 19:44:46 +0100

    add: main loop features

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import os
 
 class DynamicGrid:
 
@@ -37,6 +36,7 @@ class DynamicGrid:
 
 	def sort_row(self):
 		pass
+
 	def find_element(self, element):
 		numList = []
 		i = 0
@@ -44,44 +44,78 @@ class DynamicGrid:
 			j = 0
 			for column in row:
 				if element == column:
-					print(f"{i+1},{j+1}")
+					print(f"({i+1}, {j+1})")
 					arr = [i, j]
 					numList.append(arr)
 				j += 1
 			i += 1
+
 		if not numList:
-			print ("Element not Found")
-		
+			return True
+		else:
+			return False
+
 	def view_matrix(self):
+		print("")
 		for row in self.matrix:
 			print(row)
 
+"""
+Main function below!
+"""
 def main():
 	matrix = DynamicGrid()
 	print("Dynamic Matrix Generated!")
 
 	while True:
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("Welcome to a two-dimentional matrix processor" \
-		"1. View the matrix" \
-		"2. Total sum of the matrix" \
-		"3. Smallest number in the matrix" \
-		"4. Largest number in the matrix" \
-		"5. Sort a row" \
-		"6. Search for an element" \
-		"7. Exit" \
+		print("Welcome to a two-dimentional matrix processor\n" \
+		"1. View the matrix\n" \
+		"2. Total sum of the matrix\n" \
+		"3. Smallest number in the matrix\n" \
+		"4. Largest number in the matrix\n" \
+		"5. Sort a row\n" \
+		"6. Search for an element\n" \
+		"7. Exit\n" \
 		"")
 
 		choice = input("Enter a choice (1 - 7): ")
 		if (choice == "1"):
 			matrix.view_matrix()
+			print("")
+
 		elif (choice == "2"):
 			print(f"Total Sum: {matrix.total_sum()}")
-		elif (choice == "3"):
-			matrix.find_smallest()
-		elif (choice == "4"):
-			matrix.find_
 
+		elif (choice == "3"):
+			num = matrix.find_smallest()
+			if matrix.find_element(num):
+				print("There is no smallest number... this is a bug.")
+
+		elif (choice == "4"):
+			num = matrix.find_biggest()
+			if matrix.find_element(num):
+				print("There is no biggest number... this is a bug.")
+
+		elif (choice == "5"):
+			#
+			# enter a row here
+			#
+
+			matrix.sort_row()
+		elif (choice == "6"):
+
+			string = input("Enter an element: ")
+			if not string.isdecimal:
+				print("Invalid input. Please enter a number.")
+				continue
+
+			element = int(string)
+			matrix.find_element(element)
+		elif (choice == "7"):
+			print("Exiting...")
+			break
+		else:
+			print("Invalid choice! Please try again!")
 
 if __name__ == "__main__":
 	main()
